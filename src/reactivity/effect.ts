df7d7c01e069a02b71a3e73968506caa2cfa41d8
chenxuehai@alltuu.com <chenxuehai@alltuu.com>
2021-12-26 22:35:31 +0800

    feat: effect scheduler功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,6 +1,6 @@
 class ReactiveEffect {
   private _fn: any
-  constructor (fn) {
+  constructor (fn, public scheduler?) {
     this._fn = fn
   }
   run () {
@@ -13,8 +13,9 @@ class ReactiveEffect {
 // 用于存储当前的依赖
 let activeEffect
 
-export function effect (fn) {
-  const _effect = new ReactiveEffect(fn)
+export function effect (fn, options:any = {}) {
+  const scheduler = options.scheduler
+  const _effect = new ReactiveEffect(fn, scheduler)
   _effect.run()
   return _effect.run.bind(_effect)
 }
@@ -41,6 +42,10 @@ export function trigger (target, key) {
   const depsMap = targetMap.get(target)
   const dep = depsMap.get(key)
   for (const effect of dep) {
-    effect.run()
+    if (effect.scheduler) {
+      effect.scheduler()
+    } else {
+      effect.run()
+    }
   }
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -5,16 +5,12 @@ describe('effect', () => {
     const user = reactive({
       age: 10
     })
-
     let nextAge
-
     // 收集依赖
     effect(() => {
       nextAge = user.age + 1
     })
-
     expect(nextAge).toBe(11)
-
     // update
     user.age++
     expect(nextAge).toBe(12)
@@ -34,5 +30,37 @@ describe('effect', () => {
     expect(foo).toBe(12)
     // 验证返回值
     expect(r).toBe('foo')
+  })
+
+  // vue3中一样的测试 scheduler
+  it('scheduler', () => {
+    // 通过effect第二个参数给定一个名为scheduler的fn
+    // 当effect第一次执行时，会执行fn
+    // 当响应式对象发生set update时，不会执行fn，而是执行scheduler
+    // 如果当执行runner时，会再次执行fn
+    let dummy
+    let run: any
+    const scheduler = jest.fn(() => {
+      // runner就是effect返回的fn
+      run = runner
+    })
+    const obj = reactive({ foo: 1 })
+    const runner = effect(
+      () => {
+        dummy = obj.foo
+      },
+      { scheduler }
+    )
+    expect(scheduler).not.toHaveBeenCalled()
+    expect(dummy).toBe(1)
+    // should be called on first trigger
+    obj.foo++
+    expect(scheduler).toHaveBeenCalledTimes(1)
+    // should not run yet
+    expect(dummy).toBe(1)
+    // manually run
+    run()
+    // should have run
+    expect(dummy).toBe(2)
   })
 })
