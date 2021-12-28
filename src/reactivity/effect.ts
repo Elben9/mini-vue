dff6569e31def7dddfd44e1150075ccf34516280
chenxuehai@alltuu.com <chenxuehai@alltuu.com>
2021-12-29 00:14:23 +0800

    feat: effect stop功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,23 +1,49 @@
+import { extend } from "../util"
+
 class ReactiveEffect {
   private _fn: any
-  constructor (fn, public scheduler?) {
+  public scheduler: Function | undefined
+  onStop?: () => void
+  active = true
+  deps = []
+  constructor (fn, scheduler?: Function) {
     this._fn = fn
+    this.scheduler = scheduler
   }
   run () {
     activeEffect = this
     // this._fn()
     return this._fn()
   }
+  stop () {
+    if (this.active) {
+      cleanupEffect(this)
+      if (this.onStop) {
+        this.onStop()
+      }
+      this.active = false
+    }
+  }
+}
+
+function cleanupEffect (effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect)
+  })
 }
 
 // 用于存储当前的依赖
 let activeEffect
 
 export function effect (fn, options:any = {}) {
-  const scheduler = options.scheduler
-  const _effect = new ReactiveEffect(fn, scheduler)
+  const _effect = new ReactiveEffect(fn, options.scheduler)
+  // 1. _effect.onStop = options.onStop
+  // 2. Object.assign(_effect, options)
+  extend(_effect, options)
   _effect.run()
-  return _effect.run.bind(_effect)
+  const runner:any = _effect.run.bind(_effect)
+  runner.effect = _effect
+  return runner
 }
 
 const targetMap = new Map()
@@ -35,7 +61,11 @@ export function track (target, key) {
     depsMap.set(key, dep)
   }
 
+  if (!activeEffect) return
+  // dep存储了某个key下所有的effect传入的方法 (如foo 可能执行了多个effect， dep中就存储了很多个关于foo的effect）
   dep.add(activeEffect)
+  // 反向存储 当前effect的class中定义一个deps数组，用于存储他相关的reactive对象中的key对应的dep
+  activeEffect.deps.push(dep)
 }
 
 export function trigger (target, key) {
@@ -48,4 +78,8 @@ export function trigger (target, key) {
       effect.run()
     }
   }
+}
+
+export function stop (runner) {
+  runner.effect.stop()
 }
